897fea1969762abfd7b87c26133a5944afbab8d8
abdeyali <abdeali535353@gmail.com>
2026-01-21 20:44:24 +0530

    Fix unsupported media type issue

--- backend/emails/views.py
+++ backend/emails/views.py
@@ -1,20 +1,26 @@
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, FormParser
 from openai import OpenAI
 from django.conf import settings
 from emails.models import EmailReply
-from rest_framework.parsers import JSONParser
 
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
 @api_view(['POST'])
-@parser_classes([JSONParser])
+@parser_classes([JSONParser, FormParser])  
 def generate_reply(request):
     email_text = request.data.get('email_text')
     tone = request.data.get('tone', 'professional')
 
     if not email_text:
-        return Response({"error": "email_text is required", "received_data": request.data}, status=400)
+        return Response(
+            {
+                "error": "email_text is required",
+                "received_data": request.data
+            },
+            status=400
+        )
 
     prompt = f"""
     You are a professional email assistant.
@@ -31,11 +37,9 @@ def generate_reply(request):
     ai_reply = response.output_text
 
     EmailReply.objects.create(
-        email_text = email_text,
-        tone = tone,
-        ai_reply = ai_reply
+        email_text=email_text,
+        tone=tone,
+        ai_reply=ai_reply
     )
 
-    return Response({
-        "reply": ai_reply
-    })
+    return Response({"reply": ai_reply})
